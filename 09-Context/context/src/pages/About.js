06daf55faf28b392966486cc98b672dbc295c53c
Samara Caldas <samaracaldass@hotmail.com>
2024-05-14 18:14:10 -0300

    Update About.js

--- 09-Context/context/src/pages/About.js
+++ 09-Context/context/src/pages/About.js
@@ -1,19 +1,21 @@
-// import { useContext } from 'react';
-// import { CounterContext } from '../context/CounterContext';
+import { useContext } from "react";
 
-import { useCounterContext } from "../hooks/useCountContext";
+import { CounterContext } from "../context/CounterContext";
+
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const About = () => {
+  const { counter } = useContext(CounterContext);
 
-    // const {counter} = useContext(CounterContext);
-    const { counter } = useCounterContext();
+  // 5 - contexto mais complexo
+  const { color } = useTitleColorContext();
 
   return (
     <div>
-        <h1>About</h1>
-        <p>Valor do contador: {counter}</p>
+      <h1 style={{ color: color }}>About</h1>
+      <p>Valor contador: {counter}</p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
